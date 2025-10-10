c8c9c2df783e454b18db092bd0dcc9256169570f
rahulk67 <rahulkumawat6767@gmail.com>
2025-10-10 15:05:49 +0530

    dfg

--- frontend/src/admin-app/pages/add-matches/matches.tsx
+++ frontend/src/admin-app/pages/add-matches/matches.tsx
@@ -111,7 +111,7 @@ const MatchesPage = () => {
                   return (
                     <tr key={index}>
                       <td>{match.name}</td>
-                      {/* <td>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(new Date(match?.matchDateTime))}</td> */}
+                      <td>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' }).format(new Date(match?.matchDateTime))}</td>
                       <td>
                         <input
                           type={'checkbox'}
