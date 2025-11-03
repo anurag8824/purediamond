25c25ad944f57e7b989f85a82b393a3a03d47dcf
rahulk67 <rahulkumawat6767@gmail.com>
2025-11-03 18:44:31 +0530

    fg

--- python/src/Controller/bet.py
+++ python/src/Controller/bet.py
@@ -13,8 +13,8 @@ from flask import jsonify
 from datetime import datetime
 
 
-superNodeUrl = "http://localhost:3025/api/"
-casinoNodeUrl = "http://localhost:3025/api/"
+superNodeUrl = "https://socket3.bxpro99.xyz/api/"
+casinoNodeUrl = "https://socket3.bxpro99.xyz/api/"
 
 # superNodeUrl = "http://localhost:3025/api/"
 # casinoNodeUrl = "http://localhost:3025/api/"
