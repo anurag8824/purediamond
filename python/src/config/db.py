c6a74b1038b38a890d301a282ada0db16b85b8fd
Infayou Digital <159997668+anurag8824@users.noreply.github.com>
2025-11-04 19:08:15 +0530

    Update db.py

--- python/src/config/db.py
+++ python/src/config/db.py
@@ -4,7 +4,7 @@ import certifi
 
 # dbClient = MongoClient('mongodb+srv://infayou:JM.dHK_PpCnV7%40L@cluster0.zbf0n.mongodb.net/infa?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true')
 # dbClient = MongoClient("mongodb+srv://365infayou:Jv9lwv6csl7J1Jp5@cluster365.sxln4q8.mongodb.net/infa?retryWrites=true&w=majority&appName=Cluster365&tlsAllowInvalidCertificates=true")
-dbClient = MongoClient("mongodb+srv://exch11new:Uehlga31v7WH0Cha@exch11new.y88ido4.mongodb.net/infa?retryWrites=true&w=majority&appName=exch11new")
+dbClient = MongoClient("mongodb://admin:Diamond11123@72.61.19.197:27017/infa?authSource=admin&replicaSet=rs0")
 
 print(dbClient)
 db = dbClient.get_database()
