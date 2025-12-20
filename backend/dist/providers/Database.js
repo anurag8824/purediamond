fc741ef39c2df3d13e67cd1382267783514f0808
rahulk67 <rahulkumawat6767@gmail.com>
2025-12-20 18:28:55 +0530

    Dfg fix ui modla

--- backend/dist/providers/Database.js
+++ backend/dist/providers/Database.js
@@ -13,8 +13,8 @@ class Database {
     static init() {
         // const dsn = Locals.config().mongooseUrl
         // const dsn = Locals.config().mongooseUrl + `?retryWrites=false&replicaSet=myReplicaSet`
-        //  const dsn = "mongodb+srv://365infayou:Jv9lwv6csl7J1Jp5@cluster365.sxln4q8.mongodb.net/infa?retryWrites=true&w=majority&appName=Cluster0"
-        const dsn = "mongodb+srv://betbhainew:FARZtgmX2xkqkZhw@betbhainew.aacdoyt.mongodb.net/infa?retryWrites=true&w=majority&appName=betbhainew";
+        const dsn = "mongodb://admin:betbhaiAdmin123@72.61.18.12:27017/infa?replicaSet=rs0&authSource=admin";
+        // const dsn = "mongodb+srv://betbhainew:FARZtgmX2xkqkZhw@betbhainew.aacdoyt.mongodb.net/infa?retryWrites=true&w=majority&appName=betbhainew";
         const options = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
